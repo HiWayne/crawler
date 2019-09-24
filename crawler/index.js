fc09e803fec614765c51ef58f7741d09f24ec092
xiaotian <tianyu.liu@duitang.com>
2019-09-24 19:35:19 +0800

    click doesn't work

--- crawler/index.js
+++ crawler/index.js
@@ -6,22 +6,27 @@ const site = "https://www.duitang.com";
 const keyword = "周杰伦";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   $console.success(`OK, start go into ${site} ...`);
   await page.goto(site);
-  await page.waitFor("#kw");
+  await page.waitFor("#dt-search");
+  const closeButton = await page.$(".mask-body .tt-s a");
+  if (closeButton) {
+    closeButton.click();
+  }
   $console.success(`start write keyword "${keyword}" ...`);
   writeKeyword(page);
   async function writeKeyword(page) {
     await page.type("#kw", keyword, { delay: 20 });
-    await page.click("#dt-search form button", { delay: 20 });
+    const clickButton = await page.$("#dt-search>form>button");
+    clickButton.click();
     $console.success("start search...");
-    getSearchResult(page);
+    //getSearchResult(page);
   }
   async function getSearchResult(page) {
     await utils.waitForLoaded(page);
-    $console.success('go into search page success.')
+    $console.success("go into search page success.");
     const pictureWrapper = await page.$("#woo-holder");
     console.log("result: ", pictureWrapper);
   }
